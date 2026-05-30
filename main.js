4c25f6b41ec9fee194f30872ac6ad7c2c9010d13
taiki-engineer <taiki.m0209@outlook.jp>
2026-05-30 14:17:05 +0900

    ページ分け追加

--- main.js
+++ main.js
@@ -3,30 +3,35 @@ const scheduleList = document.querySelector(".schedule-list");
 const monthTitle = document.querySelector(".month-title");
 const input = document.querySelector(".task-input");
 const addButton = document.querySelector(".add-btn");
-const scheduleInput = document.querySelector(".task-input");
 const dateInput = document.querySelector(".date-input");
 const timeInput = document.querySelector(".time-input");
 const categorySelect = document.querySelector(".category-select");
+const titleInput = document.querySelector(".title-input");
+const memoInput = document.querySelector(".task-input");
 let selectedDate = "";
 let categoryColor = "";
+
+const today = new Date();
+
+let year = today.getFullYear();
+let month = today.getMonth();
 
 
 function createCalender() {
     // 今日の日付取得
-    const today = new Date();
 
-    const year = today.getFullYear();
+    calender.innerHTML = "";
 
-    const month = today.getMonth() +1;
+    const date = new Date(year, month +1);
 
-    const firstDay = new Date(year, month -1, 1).getDay();
+    const firstDay = new Date(year, month , 1).getDay();
     console.log(firstDay);
 
-    const lastDate = new Date(year, month, 0).getDate();
+    const lastDate = new Date(year, month +1, 0).getDate();
     console.log(lastDate);
 
     // 月表示
-    monthTitle.textContent = `${year}年${month}月`;
+    monthTitle.textContent = `${year}年${month +1}月`;
 
     // カレンダー生成
 
@@ -47,25 +52,30 @@ function createCalender() {
         
     };
 
-    const days = document.querySelectorAll(".day")
+    const totalCells = firstDay + lastDate;
 
-    days.forEach(day => {
-        day.addEventListener("click",() => {
-            const clickedDay = day.querySelector(".day-number").textContent;
-            selectedDate = clickedDay;
+    const remainingCalls = 7 - (totalCells % 7);
 
-            scheduleList.innerHTML = `
-            <h3>${clickedDay}日の予定</h3>
-            `
-        });
-    });
-    
+    if (remainingCalls < 7) {
+
+        for (let i = 0; i < remainingCalls; i++) {
+
+            calender.innerHTML += `
+            <div class="empty"></div>
+            `;
+            
+        }
+
+        
+    }
+
 };
 
 addButton.addEventListener("click", () => {
     const date = dateInput.value;
     const time = timeInput.value;
-    const scheduleText = scheduleInput.value;
+    const title = titleInput.value;
+    const memo = memoInput.value;
     const dayNumber = Number(date.split("-")[2]);
     const targetDay = document.querySelector(`[date-day="${dayNumber}"]`);
     const category = categorySelect.value;
@@ -84,14 +94,18 @@ addButton.addEventListener("click", () => {
     if (category === "プライベート") {
         categoryColor = "purple";
     }
+    if (category === "その他") {
+        categoryColor = "gray";
+    }
 
     targetDay.innerHTML += `
-        <p class="schedule-item" style="background-color:${categoryColor}">${scheduleText}</p>
+        <p class="schedule-item" style="background-color:${categoryColor}">${title}</p>
     `;
 
     console.log(category);
     console.log(categoryColor);
 });
+
 
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
@@ -116,6 +130,43 @@ nextBtn.addEventListener("click", () => {
     }
 
     createCalender();
+});
+
+const homeMenu = document.querySelector(".home-menu");
+const scheduleMenu = document.querySelector(".schedule-menu");
+const listMenu = document.querySelector(".list-menu");
+const taskMenu = document.querySelector(".task-menu");
+
+const homePage = document.querySelector(".home-page");
+const schedulePage = document.querySelector(".schedule-page");
+const listPage = document.querySelector(".list-page");
+const taskPage = document.querySelector(".task-page");
+
+function hidePages() {
+    homePage.classList.add("hidden");
+    schedulePage.classList.add("hidden");
+    listPage.classList.add("hidden");
+    taskPage.classList.add("hidden");
+}
+
+homeMenu.addEventListener("click", () => {
+    hidePages();
+    homePage.classList.remove("hidden");
+});
+
+scheduleMenu.addEventListener("click", () => {
+    hidePages();
+    schedulePage.classList.remove("hidden");
+});
+
+listMenu.addEventListener("click", () => {
+    hidePages();
+    listPage.classList.remove("hidden");
+});
+
+taskMenu.addEventListener("click", () => {
+    hidePages();
+    taskPage.classList.remove("hidden");
 });
 
 createCalender();
